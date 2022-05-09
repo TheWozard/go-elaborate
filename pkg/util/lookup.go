d9af476d05ccd52780e7795f6e6fc5a87b0d14b0
Christopher <dan620@gmail.com>
2022-05-08 22:50:28 -0400

    greatly improve lookup interface and reliability

--- pkg/util/lookup.go
+++ pkg/util/lookup.go
@@ -1,56 +1,79 @@
 package util
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
-// NewReflectLookup creates a new Lookup where the names are automatically generated using the reflected type name.
-func NewReflectLookup[T any](items ...T) *Lookup[T] {
+// NewLookup creates a new Lookup where the names of the generics are provided as keys in the map.
+// In order to generate all the reverse lookups, all generators are called at least once to get the type of the
+// generated object.
+func NewLookup[T any](items map[string]func() T) *Lookup[T] {
 	lookup := &Lookup[T]{
-		nameLookup:    map[string]T{},
+		nameLookup:    map[string]func() T{},
 		genericLookup: map[string]string{},
 	}
-	for _, item := range items {
-		name := reflect.TypeOf(item).Name()
-		lookup.nameLookup[name] = item
-		lookup.genericLookup[name] = name
-	}
-	return lookup
-}
-
-// NewNamedLookup creates a new Lookup where the names of the generics are provided as keys in the map.
-func NewNamedLookup[T any](items map[string]T) *Lookup[T] {
-	lookup := &Lookup[T]{
-		nameLookup:    map[string]T{},
-		genericLookup: map[string]string{},
-	}
-	for name, item := range items {
-		typ := reflect.TypeOf(item).Name()
-		lookup.nameLookup[name] = item
-		lookup.genericLookup[typ] = name
+	for name, generator := range items {
+		lookup.Add(name, generator)
 	}
 	return lookup
 }
 
 // Lookup generic bi-directional lookup for names and their associated generic.
-// This should not be manually instantiated, and instead use NewReflectLookup or NewNamedLookup.
+// This should not be manually instantiated, and instead use NewLookup.
 type Lookup[T any] struct {
-	nameLookup    map[string]T
+	nameLookup    map[string]func() T
 	genericLookup map[string]string
 }
 
-// LookupByName uses a string name to lookup the relevant generic and return a new instance of it.
-// In the event of a pointer, the pointer will still point to the original interfaces.
+// LookupByName uses a string name to lookup the relevant generator and return a new instance of it.
 func (l Lookup[T]) LookupByName(name string) (T, bool) {
-	if item, ok := l.nameLookup[name]; ok {
-		return item, true
+	if generator, ok := l.nameLookup[name]; ok {
+		return generator(), true
 	}
 	var zero T
 	return zero, false
 }
 
-// LookupByStruct uses the reflected type of the passed item to lookup the name this generic
-// has been given. This may just be the reflected name of the type or a unique string, depending
-// on how the lookup was created.
-func (l Lookup[T]) LookupByStruct(item T) (string, bool) {
-	name, ok := l.genericLookup[reflect.TypeOf(item).Name()]
+// LookupByType uses the reflected type of the passed item to lookup the name of its generator.
+// In the event two generators generate the same type of object the last one added will be returned
+func (l Lookup[T]) LookupByType(item T) (string, bool) {
+	id := l.genericToId(item)
+	name, ok := l.genericLookup[id]
 	return name, ok
+}
+
+// HasName returns if the lookup has a generator for the passed name.
+func (l Lookup[T]) HasName(name string) bool {
+	_, ok := l.LookupByName(name)
+	return ok
+}
+
+// HasType returns if the lookup has a name for the passed type.
+func (l Lookup[T]) HasType(item T) bool {
+	_, ok := l.LookupByType(item)
+	return ok
+}
+
+// Add adds the generator and name to the lookup.
+func (l Lookup[T]) Add(name string, generator func() T) {
+	// TODO: currently this will accept multiple of the same typ and name
+	// This will overwrite the previous holders of these values.
+	// It might be worth erroring or panicing in the event of these.
+	id := l.genericToId(generator())
+	l.nameLookup[name] = generator
+	l.genericLookup[id] = name
+}
+
+func (l Lookup[T]) genericToId(item T) string {
+	typ := reflect.TypeOf(item)
+	kind := typ.Kind()
+	if kind == reflect.Pointer {
+		return typ.Elem().Name()
+	} else if kind == reflect.Array || kind == reflect.Slice {
+		return fmt.Sprintf("[]%s", typ.Elem().Name())
+	} else if kind == reflect.Map {
+		return fmt.Sprintf("map[%s]%s", typ.Key().Name(), typ.Elem().Name())
+	}
+	return typ.Name()
 }
--- pkg/util/lookup_test.go
+++ pkg/util/lookup_test.go
@@ -5,129 +5,44 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-// There is unusual behavior for Lookup as it is using reflected type.
-// Things that might seem diffrent return the same values when looked up.
-
-func TestLookupPrimitivesName(t *testing.T) {
-	lookup := util.NewNamedLookup(map[string]int{
-		"one":   1,
-		"two":   2,
-		"three": 3,
+func TestLookupByName(t *testing.T) {
+	type CustomStruct struct{}
+	type CustomWithPointer struct{}
+	lookup := util.NewLookup(map[string]func() interface{}{
+		"one":            func() interface{} { return 1 },
+		"name":           func() interface{} { return "John Doe" },
+		"colors":         func() interface{} { return []string{"red", "green", "blue"} },
+		"custom":         func() interface{} { return CustomStruct{} },
+		"custom-pointer": func() interface{} { return &CustomWithPointer{} },
 	})
 	testCases := []struct {
-		desc   string
-		name   string
-		ok     bool
-		result int
-	}{
-		{
-			desc:   "one",
-			name:   "one",
-			ok:     true,
-			result: 1,
-		},
-		{
-			desc:   "two",
-			name:   "two",
-			ok:     true,
-			result: 2,
-		},
-		{
-			desc:   "three",
-			name:   "three",
-			ok:     true,
-			result: 3,
-		},
-		{
-			desc:   "four",
-			name:   "four",
-			ok:     false,
-			result: 0,
-		},
-	}
-	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
-			result, ok := lookup.LookupByName(tC.name)
-			assert.Equal(t, tC.ok, ok)
-			assert.Equal(t, tC.result, result)
-		})
-	}
-}
-
-func TestLookupPrimitivesType(t *testing.T) {
-	lookup := util.NewNamedLookup(map[string]int{
-		"one":   1,
-		"two":   2,
-		"three": 3,
-	})
-	testCases := []struct {
-		desc   string
-		input  int
-		ok     bool
-		result string
-	}{
-		{
-			desc:   "one",
-			input:  1,
-			ok:     true,
-			result: "three",
-		},
-		{
-			desc:   "two",
-			input:  5,
-			ok:     true,
-			result: "three",
-		},
-	}
-	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
-			result, ok := lookup.LookupByStruct(tC.input)
-			assert.Equal(t, tC.ok, ok)
-			assert.Equal(t, tC.result, result)
-		})
-	}
-}
-
-func TestLookupStructName(t *testing.T) {
-	type StructA struct{}
-	type StructB struct {
-		data string
-	}
-	lookup := util.NewReflectLookup[interface{}](StructA{}, StructB{}, StructB{data: "test"}, &StructB{data: "other"})
-	testCases := []struct {
-		desc   string
 		name   string
 		ok     bool
 		result interface{}
 	}{
 		{
-			desc:   "A",
-			name:   "StructA",
-			ok:     true,
-			result: StructA{},
+			name: "one", ok: true, result: 1,
 		},
 		{
-			desc: "B",
-			name: "StructB",
-			ok:   true,
-			result: StructB{
-				data: "test",
-			},
+			name: "name", ok: true, result: "John Doe",
 		},
 		{
-			desc: "Indirection",
-			name: "",
-			ok:   true,
-			result: &StructB{
-				data: "other",
-			},
+			name: "colors", ok: true, result: []string{"red", "green", "blue"},
+		},
+		{
+			name: "custom", ok: true, result: CustomStruct{},
+		},
+		{
+			name: "custom-pointer", ok: true, result: &CustomWithPointer{},
+		},
+		{
+			name: "missing", ok: false, result: nil,
 		},
 	}
 	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
+		t.Run(tC.name, func(t *testing.T) {
 			result, ok := lookup.LookupByName(tC.name)
 			assert.Equal(t, tC.ok, ok)
 			assert.Equal(t, tC.result, result)
@@ -135,60 +50,75 @@ func TestLookupStructName(t *testing.T) {
 	}
 }
 
-func TestLookupStructType(t *testing.T) {
-	type StructA struct{}
-	type StructB struct {
+func TestLookupByType(t *testing.T) {
+	type CustomStruct struct {
 		data string
 	}
-	lookup := util.NewNamedLookup(map[string]interface{}{
-		"a": StructA{},
-		"b": StructB{},
-		"c": StructB{data: "test"},
+	type CustomWithPointer struct {
+		other string
+	}
+	lookup := util.NewLookup(map[string]func() interface{}{
+		"one":            func() interface{} { return 1 },
+		"name":           func() interface{} { return "John Doe" },
+		"colors":         func() interface{} { return []string{"red", "green", "blue"} },
+		"custom":         func() interface{} { return CustomStruct{} },
+		"custom-pointer": func() interface{} { return &CustomWithPointer{} },
+		"map":            func() interface{} { return map[string]int{} },
 	})
 	testCases := []struct {
-		desc   string
-		input  interface{}
-		ok     bool
-		result string
+		desc  string
+		input interface{}
+		ok    bool
+		name  string
 	}{
 		{
-			desc:   "A",
-			input:  StructA{},
-			ok:     true,
-			result: "a",
+			desc: "int", input: 1, ok: true, name: "one",
 		},
 		{
-			desc:   "B",
-			input:  StructB{},
-			ok:     true,
-			result: "c",
+			desc: "diffrent int", input: 2, ok: true, name: "one",
 		},
 		{
-			desc:   "A",
-			input:  struct{}{},
-			ok:     false,
-			result: "",
+			desc: "string", input: "John Doe", ok: true, name: "name",
+		},
+		{
+			desc: "diffrent string", input: "Just some text", ok: true, name: "name",
+		},
+		{
+			desc: "string slice", input: []string{"red", "green", "blue"}, ok: true, name: "colors",
+		},
+		{
+			desc: "empty string slice", input: []string{}, ok: true, name: "colors",
+		},
+		{
+			desc: "custom struct", input: CustomStruct{}, ok: true, name: "custom",
+		},
+		{
+			desc: "modified custom struct", input: CustomStruct{data: "this one has non-zero data"}, ok: true, name: "custom",
+		},
+		{
+			desc: "custom pointer generator struct", input: CustomWithPointer{}, ok: true, name: "custom-pointer",
+		},
+		{
+			desc: "empty int slice", input: []int{}, ok: false, name: "",
+		},
+		{
+			desc: "generic struct", input: struct{}{}, ok: false, name: "",
+		},
+		{
+			desc: "generic map", input: map[string]string{}, ok: false, name: "",
+		},
+		{
+			desc: "generic map", input: map[string]int{}, ok: true, name: "map",
+		},
+		{
+			desc: "generic function", input: func() {}, ok: false, name: "",
 		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
-			result, ok := lookup.LookupByStruct(tC.input)
+			result, ok := lookup.LookupByType(tC.input)
 			assert.Equal(t, tC.ok, ok)
-			assert.Equal(t, tC.result, result)
+			assert.Equal(t, tC.name, result)
 		})
 	}
-}
-
-func TestMutationDoNotPersist(t *testing.T) {
-	type StructB struct {
-		data string
-	}
-	lookup := util.NewNamedLookup(map[string]StructB{
-		"a": {},
-	})
-	a, _ := lookup.LookupByName("a")
-	aprime, _ := lookup.LookupByName("a")
-	require.Equal(t, aprime, a)
-	a.data = "modified"
-	require.NotEqual(t, aprime, a)
 }
