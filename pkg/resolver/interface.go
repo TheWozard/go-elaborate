e066b08b4c3d2e2bfb309ae8ed85b12d7336fd8f
Christopher <dan620@gmail.com>
2022-05-08 22:59:16 -0400

    linting fixes

--- pkg/resolver/interface.go
+++ pkg/resolver/interface.go
@@ -1,12 +1,12 @@
 package resolver
 
 const (
-	resolverTypeSchema    = "schema"
-	resolverTypeMemo      = "memo"
-	resolverTypeTracker   = "tracker"
-	resolverTypeFile      = "file"
-	resolverTypeGo        = "go"
-	resolverTypeIntercept = "intercept"
+	resolverTypeSchema  = "schema"
+	resolverTypeMemo    = "memo"
+	resolverTypeTracker = "tracker"
+	resolverTypeFile    = "file"
+	resolverTypeGo      = "go"
+	// resolverTypeIntercept = "intercept"
 )
 
 // Standard interface for geting some generic data chunk to be transformed
